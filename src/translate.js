c2aab4e670d6b3ede9d864d78833f34df410e067
MaybeKapil <117200663+MaybeKapil@users.noreply.github.com>
2022-11-29 14:54:42 -0500

    Added function translateUserText()

--- src/translate.js
+++ src/translate.js
@@ -1,24 +1,26 @@
-const AWS = require("aws-sdk");
-AWS.config.update({region: "us-east-1"});
+function translateUserText() {
+  const AWS = require("aws-sdk");
+  AWS.config.update({region: "us-east-1"});
 
-const translate = new AWS.Translate();
+  const translate = new AWS.Translate();
 
-let sourceLang = document.getElementById("srcLang").value;
-let destinationLang = document.getElementById("dstLang").value;
-let srcText = document.getElementById("userTextArea").value;
+  let sourceLang = document.getElementById("srcLang").value;
+  let destinationLang = document.getElementById("dstLang").value;
+  let srcText = document.getElementById("userTextArea").value;
 
-/*
-let sourceLang = 'auto';
-let destinationLang = 'ar';
-let srcText = 'happy birthday to you';*/
+  /*
+  let sourceLang = 'auto';
+  let destinationLang = 'ar';
+  let srcText = 'happy birthday to you';*/
 
-let params = {
-  SourceLanguageCode: sourceLang,
-  TargetLanguageCode: destinationLang,
-  Text: srcText
-};
+  let params = {
+    SourceLanguageCode: sourceLang,
+    TargetLanguageCode: destinationLang,
+    Text: srcText
+  };
 
-translate.translateText(params, function (err, data) {
-  if (err) console.log(err, err.stack); 
-  else     console.log(data['TranslatedText']);
-});
+  translate.translateText(params, function (err, data) {
+    if (err) console.log(err, err.stack); 
+    else     console.log(data['TranslatedText']);
+  });
+}
