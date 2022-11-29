f99ab3726245fd48e5b6afef994ce03be3dc8c9b
MaybeKapil <kregmi1@students.towson.edu>
2022-11-29 19:45:06 +0000

    Translation script

---
+++ src/translate.js
@@ -0,0 +1,24 @@
+const AWS = require("aws-sdk");
+AWS.config.update({region: "us-east-1"});
+
+const translate = new AWS.Translate();
+
+let sourceLang = document.getElementById("srcLang").value;
+let destinationLang = document.getElementById("dstLang").value;
+let srcText = document.getElementById("userText").value;
+
+/*
+let sourceLang = 'auto';
+let destinationLang = 'ar';
+let srcText = 'happy birthday to you';*/
+
+let params = {
+  SourceLanguageCode: sourceLang,
+  TargetLanguageCode: destinationLang,
+  Text: srcText
+};
+
+translate.translateText(params, function (err, data) {
+  if (err) console.log(err, err.stack); 
+  else     console.log(data['TranslatedText']);
+});
