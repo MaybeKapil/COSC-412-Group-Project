c412a31d82abe8cc9580fd2f1afa162e6008914b
MaybeKapil <117200663+MaybeKapil@users.noreply.github.com>
2022-12-08 23:40:30 -0500

    Converts currency

---
+++ js/exchangeRate.js
@@ -0,0 +1,19 @@
+async function getExchangeRate() {
+    const access_key = 'cZG4AWhV0577FCvDaOpSbT43MGpaqHLL';
+    const from = document.getElementById("currencyFrom").value;
+    const to = document.getElementById("currencyTo").value;
+    const amount = document.getElementById("currencyAmount").value;
+    var myHeaders = new Headers();
+    myHeaders.append("apikey", "cZG4AWhV0577FCvDaOpSbT43MGpaqHLL");
+
+    var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+    };
+
+    const response = await fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`, requestOptions)
+    const data =  await response.json();
+    document.getElementById("convertedAmount").value = data.result;
+
+}
