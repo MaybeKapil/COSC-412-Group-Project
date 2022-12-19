3520cf60571ade0543cae16cfe32d8607119035a
MaybeKapil <117200663+MaybeKapil@users.noreply.github.com>
2022-12-19 00:22:55 -0500

    Updated function parameters

--- js/textToSpeech.js
+++ js/textToSpeech.js
@@ -1,27 +1,23 @@
 // Function invoked by button click
-function speakText(textAreaID, voiceLangSelectID, voiceNameSelectID) {
+function speakText(textAreaValue, voiceLangSelectValue, voiceNameSelectValue) {
     // Initialize the Amazon Cognito credentials provider
     AWS.config.region = 'us-east-1';
     AWS.config.credentials = new AWS.CognitoIdentityCredentials({
         IdentityPoolId: 'us-east-1:08cba64f-3291-4366-a2e0-4d28bd7e42a2'});
-
-    // const voiceLangCode = document.getElementById(voiceLangSelectID).value;
-    const voiceNameCode = document.getElementById(voiceNameSelectID).value;
-
-    const voiceLangCode = voiceLangSelectID;
-    //const voiceNameCode = voiceNameSelectID;
     
-    // Create the JSON parameters for getSynthesizeSpeechUrl
+    console.log(textAreaValue);
+    console.log(voiceLangSelectValue);
+    console.log(voiceNameSelectValue);
+
+    //Create the JSON parameters for getSynthesizeSpeechUrl
     const speechParams = {
         OutputFormat: "mp3",
         SampleRate: "16000",
-        Text: "",
+        Text: textAreaValue,
         TextType: "text",
-        VoiceId: "Hala",
-        LanguageCode: voiceLangCode
+        VoiceId: voiceNameSelectValue,
+        LanguageCode: voiceLangSelectValue
     };
-    // speechParams.Text = document.getElementById(textAreaID).value;
-    speechParams.Text = textAreaID;
 
      // Create the Polly service object and presigner object
      const polly = new AWS.Polly({apiVersion: '2016-06-10'});
@@ -30,11 +26,11 @@ function speakText(textAreaID, voiceLangSelectID, voiceNameSelectID) {
      // Create presigned URL of synthesized speech file
      signer.getSynthesizeSpeechUrl(speechParams, function(error, url) {
      if (error) {
-        if (textAreaID == "userTextArea") {
+        if (textAreaValue == "userTextArea") {
             document.getElementById('srcResult').style.display="block";
             document.getElementById('srcResult').innerHTML = error;
         }
-        else if (textAreaID == "outputTextArea") {
+        else if (textAreaValue == "outputTextArea") {
             document.getElementById('dstResult').style.display="block";
             document.getElementById('dstResult').innerHTML = error;
         }
