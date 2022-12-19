8c823ed45230111efc94bd16bbc07c1343087eb7
MaybeKapil <117200663+MaybeKapil@users.noreply.github.com>
2022-12-18 21:30:18 -0500

    Separate functions for dropdown population
    Currency convertor page use any of the other drops down so putting them into separate functions lows for choosing which one to activate

--- js/populateDropDown.js
+++ js/populateDropDown.js
@@ -6,65 +6,78 @@ const create = (tag = "div", options = {}, children = []) => {
     return node;
 }
 
-const selectsLang = [
-    { type: "src", def: "en-US" },
-    { type: "dst", def: "en-US" },
-];
+function populateLangDropDown() {
+    const selectsLang = [
+        { type: "src", def: "en-US" },
+        { type: "dst", def: "en-US" },
+    ];
 
-const selectsVoiceLang = [
-    { type: "src", def: "en-US" },
-    { type: "dst", def: "en-US" },
-];
-
-const selectsCurrency = [
-    { type: "currencyFrom", def: "USD" },
-    { type: "currencyTo", def: "GBP" },
-];
-
-for (const [i, { type, def }] of selectsLang.entries()) {
-    document.querySelector(`select[name=${type}Lang]`).append(
-        ...Object.entries(languages).slice(i).map(
-            ([code, name]) => {
-                const settings = { value: code, textContent: name };
-                if (code === def) settings.selected = true;
-                return create("option", settings)
-            }
-        )
-    );
+    for (const [i, { type, def }] of selectsLang.entries()) {
+        document.querySelector(`select[name=${type}Lang]`).append(
+            ...Object.entries(languages).slice(i).map(
+                ([code, name]) => {
+                    const settings = { value: code, textContent: name };
+                    if (code === def) settings.selected = true;
+                    return create("option", settings)
+                }
+            )
+        );
+    }
 }
 
-for (const [i, { type, def }] of selectsVoiceLang.entries()) {
-    document.querySelector(`select[name=${type}VoiceLang]`).append(
-        ...Object.entries(voiceLang).slice(0).map(
-            ([code, name]) => {
-                const settings = { value: code, textContent: name };
-                if (code === def) settings.selected = true;
-                return create("option", settings)
-            }
-        )
-    );
+function populateVoiceLangDropDown() {
+    const selectsVoiceLang = [
+        { type: "src", def: "en-US" },
+        { type: "dst", def: "en-US" },
+    ];
+
+    for (const [i, { type, def }] of selectsVoiceLang.entries()) {
+        document.querySelector(`select[name=${type}VoiceLang]`).append(
+            ...Object.entries(voiceLang).slice(0).map(
+                ([code, name]) => {
+                    const settings = { value: code, textContent: name };
+                    if (code === def) settings.selected = true;
+                    return create("option", settings)
+                }
+            )
+        );
+    }
 }
 
-for (const [i, { type, def }] of selectsVoiceName.entries()) {
-    document.querySelector(`select[name=${type}VoiceName]`).append(
-        ...Object.entries(voiceName).slice(0).map(
-            ([code, name]) => {
-                const settings = { value: code, textContent: name };
-                if (code === def) settings.selected = true;
-                return create("option", settings)
-            }
-        )
-    );
+function populateVoiceNameDropDown() {
+    const selectsCurrency = [
+        { type: "src", def: "Zeina" },
+        { type: "dst", def: "Zeina" },
+    ];
+
+    for (const [i, { type, def }] of selectsVoiceName.entries()) {
+        document.querySelector(`select[name=${type}VoiceName]`).append(
+            ...Object.entries(voiceName).slice(0).map(
+                ([code, name]) => {
+                    const settings = { value: code, textContent: name };
+                    if (code === def) settings.selected = true;
+                    return create("option", settings)
+                }
+            )
+        );
+    }
 }
 
-for (const [i, { type, def }] of selectsCurrency.entries()) {
-    document.querySelector(`select[name=${type}]`).append(
-        ...Object.entries(voiceName).slice(0).map(
-            ([code, name]) => {
-                const settings = { value: code, textContent: name };
-                if (code === def) settings.selected = true;
-                return create("option", settings)
-            }
-        )
-    );
+function populateCurrencyDropDown() {
+    const selectsCurrency = [
+        { type: "currencyFrom", def: "USD" },
+        { type: "currencyTo", def: "GBP" },
+    ];
+
+    for (const [i, { type, def }] of selectsCurrency.entries()) {
+        document.querySelector(`select[name=${type}]`).append(
+            ...Object.entries(currencies).slice(0).map(
+                ([code, name]) => {
+                    const settings = { value: code, textContent: name };
+                    if (code === def) settings.selected = true;
+                    return create("option", settings)
+                }
+            )
+        );
+    }
 }
