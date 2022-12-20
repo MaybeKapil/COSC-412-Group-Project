b9791eb801c359fe5a28eed86625949a74cb721f
MaybeKapil <117200663+MaybeKapil@users.noreply.github.com>
2022-12-19 22:49:26 -0500

    Removed onchange function for swapSrcAndDstLangButton

--- js/enableAutoTranslation.js
+++ js/enableAutoTranslation.js
@@ -3,20 +3,13 @@ function activateAutoTranslation(activation) {
     // Will translate automatically on input of the testbox and/or changing the selected source or destination languages (the 2 select options for text languages)
     
     if (activation == true) {
-        document.getElementById("srcLangSelect").addEventListener("change", translateUserText);   
-        document.getElementById("dstLangSelect").addEventListener("change", translateUserText);   
+        document.getElementById("srcLangSelect").addEventListener("change", translateUserText);
+        document.getElementById("dstLangSelect").addEventListener("change", translateUserText);
         document.getElementById("userTextArea").addEventListener("change", translateUserText);
     }
     else if (activation == false) {
-        document.getElementById("srcLangSelect").onchange = function() {
-            if (document.getElementById("srcLangSelect").value == "auto") {
-                document.getElementById("swapSrcAndDstLangButton").disabled = true;
-            }
-            else {
-                document.getElementById("swapSrcAndDstLangButton").disabled = false;
-            }
-        };   
-        document.getElementById("dstLangSelect").removeEventListener("change", translateUserText);   
+        document.getElementById("srcLangSelect").removeEventListener("change", translateUserText);
+        document.getElementById("dstLangSelect").removeEventListener("change", translateUserText);
         document.getElementById("userTextArea").removeEventListener("change", translateUserText);
     }
     else {
